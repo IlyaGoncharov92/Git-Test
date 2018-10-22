77df710f1adef5526395587e3d4045cdf519620b
Ilya Goncharov <ilya.goncharov@itmint.ca>
2018-10-22 17:49:54 +0300

    master commit 2

--- src/state/common/middleware.ts
+++ src/state/common/middleware.ts
@@ -3,7 +3,7 @@
 import { applyMiddleware, StoreEnhancer } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const masterFix1 = 1;
+const masterFix1 = 2;
 
 /* tslint:disable */
 
