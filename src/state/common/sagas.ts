0bce62ebf9f25b13fb6b8f440a2f2d20a0c32046
Ilya Goncharov <ilya.goncharov@itmint.ca>
2018-11-20 20:11:59 +0300

    master com 1

--- src/state/common/sagas.ts
+++ src/state/common/sagas.ts
@@ -5,4 +5,4 @@ export function promise<T>(arg: () => Promise<T>): CallEffectFactory<Promise<T>>
   return sagaCall(arg) as any as CallEffectFactory<Promise<T>>;
 }
 
-const t = 123;
+const t = 12344;
